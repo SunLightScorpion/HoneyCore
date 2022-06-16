08cbf36e55c1041f292607387204f5fd67e8351f
lukas <lukasbaehr01@gmail.com>
2022-06-16 22:44:06 +0200

    fix logical things

--- src/main/java/org/scorpion/commands/CommandSeen.java
+++ src/main/java/org/scorpion/commands/CommandSeen.java
@@ -11,18 +11,21 @@ import org.scorpion.api.HoneyAPI;
 import org.scorpion.user.HoneyUser;
 
 public class CommandSeen implements CommandExecutor {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player p) {
-            if (p.hasPermission(HoneyAPI.getPermission("seen.target"))) {
+            if (p.hasPermission(HoneyAPI.getPermission("seen-target"))) {
                 if (args.length == 1) {
                     OfflinePlayer t = Bukkit.getOfflinePlayer(args[0]);
                     HoneyUser user = new HoneyUser(p.getUniqueId());
 
+                    var data = user.getOnlineTime();
+
                     if (t.isOnline()) {
-                        p.sendMessage(HoneyAPI.getColorCode(HoneyAPI.getMessage("message.seen-target-online").replace("%target%", args[0]).replace("%time%", user.getOnlineTime())));
+                        p.sendMessage(HoneyAPI.getColorCode(HoneyAPI.getMessage("message.seen-target-online").replace("%target%", args[0]).replace("%time%", data)));
                     } else {
-                        p.sendMessage(HoneyAPI.getColorCode(HoneyAPI.getMessage("message.seen-target-offline").replace("%target%", args[0]).replace("%time%", user.getOnlineTime())));
+                        p.sendMessage(HoneyAPI.getColorCode(HoneyAPI.getMessage("message.seen-target-offline").replace("%target%", args[0]).replace("%time%", data)));
                     }
                 }
             } else {
